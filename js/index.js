b7ab76898295c6e19301f2c26509ef07c9afcda3
mattiabressan <mattiabressan1@gmail.com>
2020-08-03 23:32:33 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 
-
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
@@ -11,27 +10,16 @@ function myFunction() {
 
 
 var mySwiper = new Swiper('.swiper-container', {
-    
-  // Optional parameters
-  // slidesPerView: 3,
+
+  slidesPerView: 3,
   spaceBetween: 30,
   direction: 'horizontal',
-  //loop: true,
 
-// autoHeight:true,
-
-
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-  
     clickable: true,
-    
-
-    
   },
 
-  //Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -39,36 +27,5 @@ var mySwiper = new Swiper('.swiper-container', {
    
   },
 
-  // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//     dragSize: 7,
-//     draggable: true,
-//     hide:false
-//   },
-
- // Responsive breakpoints
- breakpoints: {
-  // when window width is >= 320px
-  320: {
-    slidesPerView: 1,
-    spaceBetween: 20
-  },
-  // when window width is >= 480px
-  700: {
-    slidesPerView: 3,
-    spaceBetween: 30
-  },
-  // when window width is >= 640px
-  1200: {
-    slidesPerView: 3,
-    spaceBetween: 30
-  }
-}
-
 })
-
-
-
-
 
